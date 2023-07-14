0a508753d8a6c0adc22604d8191a1c8600420683
Your Name <you@example.com>
2023-07-14 17:39:02 +0900

    7/14-2

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 #서버 뛰우기
-from flask import Flask , render_template
+from flask import Flask , render_template , request
 #flask 안에 Flask 객체를 가져온다.
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    os_info = dict(request.headers) #['Sec-Ch-Ua']
+    print(os_info)
     return render_template("index.html")
 
 if __name__ == '__main__':
